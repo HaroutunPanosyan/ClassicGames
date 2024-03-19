2a6042df589413a2746f9a29d1beaf6746f7b556
HaroutunPanosyan <harupanosyan@gmail.com>
2024-03-19 01:38:33 -0700

    Implemented Shuffle Feature.
    Working Version of Shuffle using a List.

--- Deck/Deck.cs
+++ Deck/Deck.cs
@@ -13,96 +13,86 @@ namespace SimpleGames.Deck
         
         public static void Reset()
         {
-            Console.WriteLine("Card(chooseSuit.Next(1,4), chooseRank.Next(2,15)");
+            Console.WriteLine("Card(chooseSuit.Next(1,4)); chooseRank.Next(2,15)");
         }
-
-        private List<Card> NewDeck()
+        // Implementation with a List.
+        public List<Card> NewDeck()
         {
-            List<Card> cards = new List<Card>
-            {
-                new Card(1, 2),
-                new Card(1, 3),
-                new Card(1, 4),
-                new Card(1, 5),
-                new Card(1, 6),
-                new Card(1, 7),
-                new Card(1, 8),
-                new Card(1, 9),
-                new Card(1, 10),
-                new Card(1, 12),
-                new Card(1, 13),
-                new Card(1, 14),
-                new Card(2, 2),
-                new Card(2, 3),
-                new Card(2, 4),
-                new Card(2, 5),
-                new Card(2, 6),
-                new Card(2, 7),
-                new Card(2, 8),
-                new Card(2, 9),
-                new Card(2, 10),
-                new Card(2, 11),
-                new Card(2, 12),
-                new Card(2, 13),
-                new Card(2, 14),
-                new Card(3, 2),
-                new Card(3, 3),
-                new Card(3, 4),
-                new Card(3, 5),
-                new Card(3, 6),
-                new Card(3, 7),
-                new Card(3, 8),
-                new Card(3, 9),
-                new Card(3, 10),
-                new Card(3, 11),
-                new Card(3, 12),
-                new Card(3, 13),
-                new Card(3, 14),
-                new Card(4, 2),
-                new Card(4, 3),
-                new Card(4, 4),
-                new Card(4, 5),
-                new Card(4, 6),
-                new Card(4, 7),
-                new Card(4, 8),
-                new Card(4, 9),
-                new Card(4, 10),
-                new Card(4, 11),
-                new Card(4, 12),
-                new Card(4, 13),
-                new Card(4, 14)
-            };
+            List<Card> cards = new List<Card>(52);
+
+            #region ManuallyCardAddition
+            // Manually adding each card.
+            cards.Add(new Card(1, 2));
+            cards.Add(new Card(1, 3));
+            cards.Add(new Card(1, 4));
+            cards.Add(new Card(1, 5));
+            cards.Add(new Card(1, 6));
+            cards.Add(new Card(1, 7));
+            cards.Add(new Card(1, 8));
+            cards.Add(new Card(1, 9));
+            cards.Add(new Card(1, 10));
+            cards.Add(new Card(1, 11));
+            cards.Add(new Card(1, 12));
+            cards.Add(new Card(1, 13));
+            cards.Add(new Card(1, 14));
+            cards.Add(new Card(2, 2));
+            cards.Add(new Card(2, 3));
+            cards.Add(new Card(2, 4));
+            cards.Add(new Card(2, 5));
+            cards.Add(new Card(2, 6));
+            cards.Add(new Card(2, 7));
+            cards.Add(new Card(2, 8));
+            cards.Add(new Card(2, 9));
+            cards.Add(new Card(2, 10));
+            cards.Add(new Card(2, 11));
+            cards.Add(new Card(2, 12));
+            cards.Add(new Card(2, 13));
+            cards.Add(new Card(2, 14));
+            cards.Add(new Card(3, 2));
+            cards.Add(new Card(3, 3));
+            cards.Add(new Card(3, 4));
+            cards.Add(new Card(3, 5));
+            cards.Add(new Card(3, 6));
+            cards.Add(new Card(3, 7));
+            cards.Add(new Card(3, 8));
+            cards.Add(new Card(3, 9));
+            cards.Add(new Card(3, 10));
+            cards.Add(new Card(3, 11));
+            cards.Add(new Card(3, 12));
+            cards.Add(new Card(3, 13));
+            cards.Add(new Card(3, 14));
+            cards.Add(new Card(4, 2));
+            cards.Add(new Card(4, 3));
+            cards.Add(new Card(4, 4));
+            cards.Add(new Card(4, 5));
+            cards.Add(new Card(4, 6));
+            cards.Add(new Card(4, 7));
+            cards.Add(new Card(4, 8));
+            cards.Add(new Card(4, 9));
+            cards.Add(new Card(4, 10));
+            cards.Add(new Card(4, 11));
+            cards.Add(new Card(4, 12));
+            cards.Add(new Card(4, 13));
+            cards.Add(new Card(4, 14));
+            #endregion
+            // Adding each card with a loop.
+
 
             return cards;
         }
+        // Implementation with a Dictionary
 
-        public HashSet<Card> Shuffle()
+        public HashSet<int> Shuffle()
         {
+            HashSet<int> shuffledNumbers = new HashSet<int>(52);
 
-            var deck = new Deck();
-            var fullDeck = deck.NewDeck();
-            HashSet<Card> shuffledCards = new HashSet<Card>();
-
-            while (shuffledCards.Count < 52) 
-            {                
-                Random random = new Random();
-                int number = random.Next(2, 52);           
-
-                if (shuffledCards.Count != 52)
-                { 
-                    if (!fullDeck.Contains(fullDeck[number]))
-                    {
-                        number = random.Next(2, 52);
-                    }
-                    shuffledCards.Add(fullDeck[number]);
-                    fullDeck.Remove(fullDeck[number]);
-                }
-                else
-                {
-                    break;
-                }
+            while (shuffledNumbers.Count < 52)
+            {
+                Random random = new Random();                
+                int number = random.Next(0, 52);
+                shuffledNumbers.Add(number);                
             }
-            return shuffledCards;
+            return shuffledNumbers;
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using SimpleGames.Deck;
+using System;
 
 namespace Games
 
@@ -8,12 +9,29 @@ namespace Games
         static void Main(string[] args)
         {
             var deck = new Deck();
-            var newDeck = deck.Shuffle();
+            var newDeck = deck.NewDeck();
+            var shuffle = deck.Shuffle();
 
-            foreach (var num in newDeck)
+            foreach (var num in shuffle)
             {
-                Console.WriteLine($"{(RankEnum.Rank)(num.Rank)} of {(SuitEnum.Suit)num.Suit}");
+                Console.WriteLine($"{(RankEnum.Rank)newDeck[num].Rank} of {(SuitEnum.Suit)newDeck[num].Suit}");
             }
+
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine($"First Card is: {(RankEnum.Rank)newDeck[shuffle.FirstOrDefault()].Rank} of {(SuitEnum.Suit)newDeck[shuffle.FirstOrDefault()].Suit} ");
+            shuffle.Remove(shuffle.FirstOrDefault());
+            Console.WriteLine($"Second Card is: {(RankEnum.Rank)newDeck[shuffle.FirstOrDefault()].Rank} of {(SuitEnum.Suit)newDeck[shuffle.FirstOrDefault()].Suit} ");
+            shuffle.Remove(shuffle.FirstOrDefault());
+            Console.WriteLine($"Third Card is: {(RankEnum.Rank)newDeck[shuffle.FirstOrDefault()].Rank} of {(SuitEnum.Suit)newDeck[shuffle.FirstOrDefault()].Suit} ");
+            shuffle.Remove(shuffle.FirstOrDefault());
+            Console.WriteLine($"Fourth Card is: {(RankEnum.Rank)newDeck[shuffle.FirstOrDefault()].Rank} of {(SuitEnum.Suit)newDeck[shuffle.FirstOrDefault()].Suit} ");
+            shuffle.Remove(shuffle.FirstOrDefault());
+            Console.WriteLine($"Fifth Card is: {(RankEnum.Rank)newDeck[shuffle.FirstOrDefault()].Rank} of {(SuitEnum.Suit)newDeck[shuffle.FirstOrDefault()].Suit} ");
+            shuffle.Remove(shuffle.FirstOrDefault());
+            Console.WriteLine($"Sixth Card is: {(RankEnum.Rank)newDeck[shuffle.FirstOrDefault()].Rank} of {(SuitEnum.Suit)newDeck[shuffle.FirstOrDefault()].Suit} ");
+            Console.WriteLine();
 
         }
     }
